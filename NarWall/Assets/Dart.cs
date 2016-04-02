6963df21555d0f8a13a8906b9ebc283adcc3bd4b
Stefanie Alfonso <astef@seas.upenn.edu>
2016-04-02 18:55:38 -0400

    narwal canon

--- NarWall/Assets/Dart.cs
+++ NarWall/Assets/Dart.cs
@@ -9,12 +9,22 @@ public class Dart : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        GetComponent<Rigidbody>().detectCollisions = false;
     }
 
     void updateDartRotation()
     {
+        print("update rotation");
+        var v3 = Input.mousePosition;
+        v3.z = 10.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
 
+        float yRotation = v3.x*15f;
+        float xRotation = -v3.y*15f;
+        print("x rotation" + xRotation);
+
+        this.GetComponent<Rigidbody>().rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        
     }
 
     // Update is called once per frame
@@ -25,9 +35,12 @@ public class Dart : MonoBehaviour
         {
             startDrag = false;
             GameObject shoot = (GameObject)Instantiate(ammo, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
-            shoot.GetComponent<Rigidbody>().AddRelativeForce(0f, 0f, 1f);
+            shoot.GetComponent<Rigidbody>().detectCollisions = true;
+            shoot.GetComponent<Rigidbody>().velocity = -20 * shoot.transform.forward;
+          //  shoot.GetComponent<Rigidbody>().AddRelativeForce(0f, 0f, -50f);
+           // shoot.GetComponent<Rigidbody>().AddRelativeForce(0f, -3*9.81f, 0f);
         }
-        else if (Input.GetMouseButtonDown(0))
+        else if (Input.GetMouseButtonDown(0) || startDrag)
         {
             updateDartRotation();
             startDrag = true;
