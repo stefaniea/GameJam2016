9d1a7b728c6f5a1ad3903d1f46228b7a6ac387e0
Stefanie Alfonso <astef@seas.upenn.edu>
2016-04-02 19:15:31 -0400

    destroy narwhal

---
+++ NarWall/Assets/Wall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("collide with wall");
+        Destroy(collision.gameObject);
+    }
+}
