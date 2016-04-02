9e52564d56b84538b10dbaf2611ac97ca7949bf8
Stefanie Alfonso <astef@seas.upenn.edu>
2016-04-02 19:04:01 -0400

    shoooot

--- NarWall/Assets/DartScript.cs
+++ NarWall/Assets/DartScript.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DartScript : MonoBehaviour {
+public class DartScript : MonoBehaviour
+{
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+	    rb = this.GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        
+        // align dart with velocity vector
+        Vector3 vel = rb.velocity;
+        rb.rotation = Quaternion.LookRotation(vel);
+        print("update rotation vel is " + vel.x + " " + vel.y + " " + vel.z);
+    }
 }
