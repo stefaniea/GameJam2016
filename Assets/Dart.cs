53e489f850e2c845b6028a1c4d0013a266213f11
Stefanie Alfonso <astef@seas.upenn.edu>
2016-04-02 17:22:27 -0400

    removed extra assets and mouse down thing

---
+++ Assets/Dart.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dart : MonoBehaviour
+{
+    public GameObject ammo;
+    public bool startDrag = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void updateDartRotation()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0) && startDrag)
+        {
+            startDrag = false;
+            GameObject shoot = (GameObject)Instantiate(ammo, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+            shoot.GetComponent<Rigidbody>().AddRelativeForce(0f, 0f, 1f);
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            updateDartRotation();
+            startDrag = true;
+        }
+    }
+}
