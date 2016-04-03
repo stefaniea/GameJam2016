4878c4c4f3fb1dc8d88efebb0303fbad7bc0a92a
Stefanie Alfonso <astef@seas.upenn.edu>
2016-04-02 20:38:54 -0400

    narwhal

--- NarWall/Assets/Wall.cs
+++ NarWall/Assets/Wall.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Wall : MonoBehaviour {
+    public Text time;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +12,7 @@ public class Wall : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        time.text = Time.time.ToString();
 	}
 
     void OnCollisionEnter(Collision collision)
